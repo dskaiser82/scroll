81d572b6a7018b3ae486104d48ba03358876035e
Dan Kaiser <dskaiser82@gmail.com>
2017-09-05 22:06:50 -0700

    first cc

---
+++ app.js
@@ -0,0 +1,7 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+        document.getElementById("myImg").className = "slideUp";
+    }
+}
